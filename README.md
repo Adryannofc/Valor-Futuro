acc8481ff28a06cf8cde27b20e3f961b8f6739d1
Pedro Kleinhans <pedrokleinhans@gmail.com>
2025-10-21 19:54:50 -0300

    fix(html/css): Descrições ajustadas, iniciado tentativo de animacao nos cards

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Valor-Futuro
+
---
+++ modulos/js/createcard.js
@@ -0,0 +1,20 @@
+function createElement(card)
+{
+    const container = document.querySelector('.cards-container')
+    const card = document.createElement('class')
+    cardContainer.classList.add('cards')
+
+
+
+
+}
+
+
+
+//  <div class="cards-container">
+//             <div class="card">
+//                 <img src="../assets/img/man-with-sales-woman-car-showroom-convertido-de-jpg.webp" alt="Consórcios">
+//                 <h3>Consórcios</h3>
+//                 <p>Para automoveis, imoveis e serviços.</p>
+//                 <button>Saiba mais</button>
+//             </div>
