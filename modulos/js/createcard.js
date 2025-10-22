3653291baf6396a1461b028e2c51382a227079bc
Pedro Kleinhans <pedrokleinhans@gmail.com>
2025-10-21 21:39:22 -0300

    feat(createcard function): função que cria card

--- modulos/js/createcard.js
+++ modulos/js/createcard.js
@@ -1,8 +1,29 @@
-function createElement(card)
+export function createCard(img,titulo,descricao,button)
 {
     const container = document.querySelector('.cards-container')
-    const card = document.createElement('class')
-    cardContainer.classList.add('cards')
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    
+    const img = document.createElement('img');
+    img.classList.add('card-img'); 
+    img.src = img;
+    img.alt = '';
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('card-title');
+    h3.textContent = titulo;
+
+    const p = document.createElement('p');
+    p.classList.add('card-p')
+    p.textContent = descricao;
+
+    const button = document.createElement('button');
+    button.classList.add('card-button');
+    button.textContent = 'Saiba mais'
+
+    container.appendChild(card);
+
 
 
 
---
+++ modulos/main.js
@@ -0,0 +1,3 @@
+import {createCard} from '../modulos/js/createcard.js';
+
+createCard("teste","teste","teste","teste");
